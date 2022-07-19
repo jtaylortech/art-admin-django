231a8b53fa0ea8e2ea7aa629f8425fe06c086d0d
Jarred Taylor <jarrede20@gmail.com>
2022-07-19 18:35:13 -0400

    registering each model that Django should represent in the admin iinterface

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from core.models import Location, Artwork, Description, ArtDiscovery, Image
 
-# Register your models here.
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Artwork)
+class ArtworkAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Description)
+class DescriptionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ArtDiscovery)
+class ArtDiscoverAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
